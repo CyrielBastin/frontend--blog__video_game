8809cd1188a44922ce583df78fe6fa7c75011c9c
Cyriel Bastin <cyriel.bastin@zoho.com>
2020-10-02 06:54:59 +0200

    blueprint for the navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeContentComponent } from './home-content/home-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeContentComponent
   ],
   imports: [
     BrowserModule,
