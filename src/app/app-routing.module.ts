877f255a52043991802344bd5a75c96cc708537c
Cyriel Bastin <cyriel.bastin@zoho.com>
2020-10-03 22:23:46 +0200

    add -- articles-page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticlesPageComponent } from './articles-page/articles-page.component';
+import { HomeContentComponent } from './home-content/home-content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'articles', component: ArticlesPageComponent },
+  { path: '', component: HomeContentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeContentComponent } from './home-content/home-content.component';
+import { ArticlesPageComponent } from './articles-page/articles-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeContentComponent
+    HomeContentComponent,
+    ArticlesPageComponent
   ],
   imports: [
     BrowserModule,
